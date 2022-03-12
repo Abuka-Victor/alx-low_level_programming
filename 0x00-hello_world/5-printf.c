f701c797ad3f2af779063e2154c2afd6c109f503
Victor Abuka <abukavictoro@gmail.com>
2022-03-12 02:13:30 -0800

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - Prints some text to the standard output
+ * Return: exit status is zero if successful
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
