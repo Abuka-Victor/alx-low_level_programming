a28a4a9e244ff93942611509e9121ab29684e1f6
Victor Abuka <abukavictoro@gmail.com>
2022-03-12 14:04:19 -0800

    V2 fix for last digit task

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,11 +11,11 @@
 int main(void)
 {
 	int n;
+	int last_digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	int last_digit;
 	last_digit = n % 10;
 
 	printf("Last digit of %d is %d", n, last_digit);
