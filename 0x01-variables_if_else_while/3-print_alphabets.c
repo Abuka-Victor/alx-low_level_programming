45db3cdf81ca67bf4b5bd1244d00e08b5dd48fbf
Victor Abuka <abukavictoro@gmail.com>
2022-03-12 14:42:35 -0800

    Print lower and upper case alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints a-z to std output
+ * Return: Exit status is zero if successful
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+	for (i = 65; i <= 90; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
