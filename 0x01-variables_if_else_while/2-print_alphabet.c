142a3a11bef8f16e9ff1714e0ec7e1aa5d3d726c
Victor Abuka <abukavictoro@gmail.com>
2022-03-12 14:21:18 -0800

    Add task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints a-z to std output
+ * Return: Exit status is zero if successful
+ */
+int main(void)
+{
+	for (int i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
