a80b8541fa033aad74851f4f94696dc6956d9d61
Victor Abuka <abukavictoro@gmail.com>
2022-03-12 14:00:33 -0800

    Include last digit task to repo

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - This will print some output based on some conditions of the
+ * last digit in a given number
+ * Return: exit status is zero if successful
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	int last_digit = n % 10;
+
+	printf("Last digit of %d is %d", n, last_digit);
+	if (last_digit == 0)
+	{
+		printf("and is 0\n");
+	}
+	else if (last_digit > 5)
+	{
+		printf("and is greater than 5\n");
+	}
+	else if (last_digit < 6 && last_digit != 0)
+	{
+		printf("and is less than 6 and not 0\n");
+	}
+	return (0);
+}
