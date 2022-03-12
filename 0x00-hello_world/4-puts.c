97f939c16cbe640a821842d2468de1e2478e7e94
Victor Abuka <abukavictoro@gmail.com>
2022-03-12 01:45:44 -0800

    Betty fix for 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 
 /*
- *main - Entry Point
- *
- * Return: Always 0 (Success)
- * */
-
+ * main - prints to the screen using puts function
+ * Return: If successful the exit status is zero
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
