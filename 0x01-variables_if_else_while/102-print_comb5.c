98185703afb61be0d3722bfa46e1bd957762da87
Victor Abuka <abukavictoro@gmail.com>
2022-03-13 12:02:01 -0700

    Initial attempt q12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints all decimal
+ * digits without using printf
+ * Return: Exit status is zero if successful
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int n;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = 48; j <= 57; j++)
+		{
+			for (k = 48; k <= 57; k++)
+			{
+				for (n = 49; n <= 57; n++)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(32);
+					putchar(k);
+					putchar(n);
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
