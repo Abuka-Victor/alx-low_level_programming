fbcca92cc647c292aaf55e8b45b1a9c3efd20d22
Victor Abuka <abukavictoro@gmail.com>
2022-03-15 00:56:39 -0700

    Task 12 correct output

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,25 +11,57 @@ int main(void)
 	int j;
 	int k;
 	int n;
+	int k_last;
+	int n_last;
 
 	for (i = 48; i <= 57; i++)
 	{
 		for (j = 48; j <= 57; j++)
 		{
+			if ((i == 57) && (j == 57))
+				break;
 			for (k = 48; k <= 57; k++)
 			{
-				if (k < i)
-					continue;
 				for (n = 48; n <= 57; n++)
 				{
+					if ((i == 48) && (j == 48))
+					{
+						if ((k == 48) && (n == 48))
+							continue;
+					}
+					if (k < i)
+					{
+						k++;
+						continue;
+					}
+					if ((k_last == 57) && (n_last == 57))
+					{
+						if ((j == 57))
+						{
+							k = i + 1;
+							n = 48;
+						}
+						else
+						{
+							k = i;
+							n = j + 1;
+						}
+					}
 					putchar(i);
 					putchar(j);
 					putchar(32);
 					putchar(k);
 					putchar(n);
+					n_last = n;
+					if ((i == 57) && (j == 56))
+					{
+						if ((k == 57) && (n == 57))
+							break;
+					}
 					putchar(44);
 					putchar(32);
 				}
+				k_last = k;
 			}
 		}
 	}
