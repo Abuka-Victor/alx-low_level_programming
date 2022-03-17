1bbe6f53e8e6b3b22d969b73758845d83233eb90
Victor Abuka <abukavictoro@gmail.com>
2022-03-17 07:04:11 -0700

    task 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,8 +2,8 @@
 
 /**
  * add - Sums up two numbers
- * @num1 - This is an integer
- * @num2 - This is also an integer
+ * @num1: This is an integer
+ * @num2: This is also an integer
  *
  * Return: The sum
  */
