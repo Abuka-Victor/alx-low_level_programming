b5f6df137cefc662a49257e1b4fc64218db3ad1f
Victor Abuka <abukavictoro@gmail.com>
2022-03-17 01:54:50 -0700

    Initial Commit for this Dir

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - program to print putchar to the screen
+ * Return: exit status is zero if successful
+ */
+int main(void)
+{
+
+	printf("_putchar\n");
+
+	return (0);
+}
