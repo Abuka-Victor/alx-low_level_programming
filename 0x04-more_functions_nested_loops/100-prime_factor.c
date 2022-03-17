242065a77876fb68bfa926081b17bffeba5d3ece
Victor Abuka <abukavictoro@gmail.com>
2022-03-17 11:54:42 -0700

    Adv task 10

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -6,13 +6,13 @@
  */
 int main(void)
 {
-    unsigned long int i = 3, n = 612852475143;
+	unsigned long int i = 3, n = 612852475143;
 
-    for (; i < 12057; i += 2)
-    {
-        while (n % i == 0 && n != i)
-            n /= i;
-    }
-    printf("%lu\n", n);
-    return (0);
+	for (; i < 12057; i += 2)
+	{
+		while (n % i == 0 && n != i)
+			n /= i;
+	}
+	printf("%lu\n", n);
+	return (0);
 }
