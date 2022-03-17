af77a47e3968b331c6309b21ee913afa0516011f
Victor Abuka <abukavictoro@gmail.com>
2022-03-17 07:29:22 -0700

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -21,7 +21,7 @@ void print_to_98(int n)
 			}
 			else
 			{
-				printf("%d\n");
+				printf("%d\n", i);
 			}
 		}
 	}
@@ -35,7 +35,7 @@ void print_to_98(int n)
 			}
 			else
 			{
-				printf("%d\n");
+				printf("%d\n", i);
 			}
 		}
 	}
