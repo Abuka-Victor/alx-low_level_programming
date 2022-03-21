012aa07585aff8a3abc15406a864c652e2da2528
Victor Abuka <abukavictoro@gmail.com>
2022-03-21 12:03:58 -0700

    Task 4

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,10 +11,10 @@
 void rev_string(char *s)
 {
 	int track;
-	int index;
+	int index = 0;
 	char *backup_str = s;
 
-	for (index = -1, track = _strlen(s) - 1; track >= 0; track--)
+	for (track = _strlen(s); track >= 0; track--)
 	{
 		*(s + index) = *(backup_str + track);
 		index++;
