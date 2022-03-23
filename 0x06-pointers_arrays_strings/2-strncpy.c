fac478f8292f2ce9160dbc165b23e2f9022ee3bd
Victor Abuka <abukavictoro@gmail.com>
2022-03-23 13:05:28 -0700

    task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,21 +11,13 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-	int counter = 0;
-	int srcLen = 0;
-
-	while (src[counter] != '\0')
-	{
-		srcLen++;
-		counter++;
-	}
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
 
-	if (n > srcLen)
+	for (; i < n; i++)
 		dest[i] = '\0';
 
 	return (dest);
