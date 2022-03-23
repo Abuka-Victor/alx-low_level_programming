4596149ec499710ce011d24ae9caa4766bef286a
Victor Abuka <abukavictoro@gmail.com>
2022-03-23 16:50:22 -0700

    Task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -13,8 +13,10 @@ char *string_toupper(char *n)
 
 	for (i = 0; n[i] != '\0'; i++)
 	{
-		if (n[i] >= 97 || n[i] <= 122)
-			n[i] -= 32;
+		if (n[i] >= 97 && n[i] <= 122)
+			n[i] = n[i] - 32;
+		else
+			continue;
 	}
 
 	return (n);
