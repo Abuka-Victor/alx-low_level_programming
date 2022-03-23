35c4668fbc2bc2092072744f152509961002d409
Victor Abuka <abukavictoro@gmail.com>
2022-03-23 10:30:16 -0700

    Initialization of dir 6 and task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcat - Concatenates one string to another
+ * @dest: This is the string to be concatenated to
+ * @src: This is the string to be concatenated
+ *
+ * Return: The destination string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int destLength = 0;
+
+	while (dest)
+	{
+		destLength++;
+	}
+
+	for (i = 0; src[i] != '\n'; i++)
+	{
+		dest[destLength + i] = src[i];
+	}
+	dest[destLength + i] = '\n';
+
+	return (dest);
+}
