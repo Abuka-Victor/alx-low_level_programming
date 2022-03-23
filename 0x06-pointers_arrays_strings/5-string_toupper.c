18e01a29e367e0a1e466d6b65dce6ae0d0afdbc0
Victor Abuka <abukavictoro@gmail.com>
2022-03-23 16:38:02 -0700

    Task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - Changes any lowercase letter
+ * in a string to uppercase
+ * @n: The string to check
+ *
+ * Return: The uppercased string
+ */
+char *string_toupper(char *n)
+{
+	int i;
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		if (n[i] >= 97 || n[i] <= 122)
+			n[i] -= 32;
+	}
+
+	return (n);
+}
