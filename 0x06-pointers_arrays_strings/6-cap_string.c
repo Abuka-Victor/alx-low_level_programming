9775a53e19156f583db5081eededad0b03817eb5
Victor Abuka <abukavictoro@gmail.com>
2022-03-23 17:14:43 -0700

    Task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * cap_string - This Capitalizes any begining of a word
+ * @n: This is the string
+ *
+ * Return: Gives back the capitalized string
+ */
+char *cap_string(char *n)
+{
+	char sep[] = {'\n', '\t', ' ', 40, 41, 44, 59, 46, 33, 63, 34, 123, 125};
+	int i, j;
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		for (j = 0; j < 13; j++)
+		{
+			if (n[i] == sep[j])
+			{
+				if (n[i + 1] >= 97 && n[i + 1] <= 122)
+					n[i + 1] -= 32;
+			}
+		}
+	}
+
+	return (n);
+}
