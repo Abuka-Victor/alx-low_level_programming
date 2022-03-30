61e4e79c286052c407047e217b6b1f679b9d440b
Victor Abuka <abukavictoro@gmail.com>
2022-03-30 14:32:56 -0700

    0x08

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.c"
+
+/**
+ * _print_rev_recursion - Prints a string recursively in reverse
+ * @s: This is the string
+ *
+ * Return: Null void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
