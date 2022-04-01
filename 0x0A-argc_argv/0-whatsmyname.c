792026a68f7999733eebd54de75b316ed6edcb32
Victor Abuka <abukavictoro@gmail.com>
2022-04-01 14:57:34 -0700

    task argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints its name
+ * @argc: Argument count
+ * @argv: Argument Vector
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - argc]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints all its args
+ * @argc: Argument count
+ * @argv: Argument Vector
+ *
+ * Return: Always 0
+ */
+
+int main(int argc,  char *argv[])
+{
+	while (argc--)
+		printf("%s\n", *argv++);
+	return (0);
+}
