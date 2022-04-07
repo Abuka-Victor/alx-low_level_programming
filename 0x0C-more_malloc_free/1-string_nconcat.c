c19a55887daa70b02a69bb7aaa8beddce6e771a6
Victor Abuka <abukavictoro@gmail.com>
2022-04-07 16:03:08 -0700

    Task 0

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - Concats 2 strings up to a no of bytes
+ * @s1: The first string
+ * @s2: The second string
+ * @n: The cap
+ *
+ * Return: A pointer to a string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *newString;
+	unsigned int size1, size2, i, j;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	size1 = sizeof s1;
+	size2 = sizeof s2;
+
+	if (n > size2)
+		n = size2--;
+
+	newString = malloc(size1 + n);
+	if (newString == NULL)
+		return (NULL);
+
+	for (i = 0; s1[i] != '\0'; i++)
+		newString[i] = s1[i];
+
+	for (j = 0; j < n; j++, i++)
+		newString[i] = s2[i];
+	newString[i] = '\0';
+
+	return (newString);
+}
