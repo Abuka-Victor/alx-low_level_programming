99bfd70869338dd5d798873a6fea54c17e224342
Victor Abuka <abukavictoro@gmail.com>
2022-04-07 15:41:19 -0700

    Task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,6 +4,8 @@
 /**
  * malloc_checked - This provides a safe way of mallocing
  * @b: This is the number of bytes to be malloced
+ *
+ * Return: Void Pointer
  */
 void *malloc_checked(unsigned int b)
 {
