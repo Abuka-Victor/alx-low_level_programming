ee992ee11c8ef3d75dbd1a0ed2f2e063957793b7
Victor Abuka <abukavictoro@gmail.com>
2022-04-07 18:53:34 -0700

    Adv 2

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - This prints multiplication of 2 numbers
+ * @argc: This is the argument count
+ * @argv: This is the argument vector
+ *
+ * Return - Always 0
+ */
+int main(int argc, char **argv)
+{
+	int num1, num2;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	if (num1 == 0 || num2 == 0)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	else
+	{
+		printf("%d\n", num1 * num2);
+	}
+
+	return (0);
+}
