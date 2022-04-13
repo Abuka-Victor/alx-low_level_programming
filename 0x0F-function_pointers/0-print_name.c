f67fb3ff2f8fa7a8587284dce11fb3ae65cb9462
Abuka-Victor <abukavictoro@gmail.com>
2022-04-13 22:57:42 +0100

    Add NULL pointer catch to task 0 and Solve Task 1

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stddef.h>
 
 /**
  * print_name - Prints a name
@@ -9,5 +10,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+		return;
 	f(name);
 }
