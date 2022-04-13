57f53f33d82d662c71ad73facd97013ca53d78fd
Abuka-Victor <abukavictoro@gmail.com>
2022-04-13 22:30:17 +0100

    Add include for stdio.h dependency

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
 #ifndef POINTER_FUNC
 #define POINTER_FUNC
 
+#include <stdio.h>
+
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
