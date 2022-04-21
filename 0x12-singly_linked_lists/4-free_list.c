783705b5bc8883f3d827d3bb2765905a2aead993
Abuka-Victor <abukavictoro@gmail.com>
2022-04-21 20:18:37 +0100

    Tasks

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * free_list - Frees a list
+ * @head: The head of the list
+ *
+ * Return: Null void
+ */
+void free_list(list_t *head)
+{
+	list_t *node;
+	list_t *forward;
+	node = head;
+	forward = node->next;
+
+	while (node != NULL)
+	{
+		free(node);
+		node = forward;
+		if (forward->next != NULL)
+			forward = forward->next;
+		else
+		{
+			free(forward);
+			break;
+		}
+	}
+}
