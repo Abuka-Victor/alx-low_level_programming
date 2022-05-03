b2158eb7314fdc06475c4fd79cf0a556c214943b
Victor Abuka <abukavictoro@gmail.com>
2022-05-02 17:31:59 -0700

    [Fix] Betty Taks 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-		for (len = 0; text_content[len] != '\0'; len++);
+		for (len = 0; text_content[len] != '\0'; len++)
+			;
 		write(fd, text_content, len);
 	}
 
