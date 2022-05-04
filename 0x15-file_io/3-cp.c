92ee9e51cbe7b89543060f58e5830570b006ba74
Abuka-Victor <abukavictoro@gmail.com>
2022-05-05 00:57:39 +0100

    Task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,36 @@
 #include "main.h"
 
+
+
+/**
+ * exitRead - Exits the code with a status
+ * @status: The status code to exit with
+ * @av: The argument Vector
+ * @fileNo: The filenumber
+ *
+ * Return: Null void
+ */
+void exitRead(int status, char *av[], int fileNo)
+{
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[fileNo]);
+	exit(status);
+}
+
+/**
+ * exitWrite - Exits the code with a status
+ * @status: The status code to exit with
+ * @av: The argument Vector
+ * @fileNo: The filenumber
+ *
+ * Return: Null void
+ */
+void exitWrite(int status, char *av[], int fileNo)
+{
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[fileNo]);
+	exit(status);
+}
+
+
 /**
  * main -  a program that copies the content of a file to another file.
  * @ac: Argument count
@@ -20,20 +51,19 @@ int main(int ac, char *av[])
 
 	fdf = open(av[1], O_RDONLY);
 	if (fdf == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
+		exitRead(98, av, 1);
 
 	fdt = open(av[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
 	if (fdt == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit(99);
-	}
+		exitWrite(99, av, 2);
 
 	while ((sz = read(fdf, buf, 1024)))
-		write(fdt, buf, sz);
+	{
+		if (sz == -1)
+			exitRead(98, av, 1);
+		if ((write(fdt, buf, sz)) == -1)
+			exitWrite(99, av, 2);
+	}
 	cf = close(fdf);
 	ct = close(fdt);
 	if (cf)
