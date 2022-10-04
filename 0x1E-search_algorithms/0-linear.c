995e98b2af3f7380d958811413b6ba73160b8e19
Abuka-Victor <abukavictoro@gmail.com>
2022-10-04 04:35:01 +0100

    task 0

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,23 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - A function to implement the linear search algo
+ * @array: the array to be searched
+ * @size: The size of the array
+ * @value: The value to be found
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+	if (!array)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (array[i]);
+	}
+
+	return (-1);
+}
