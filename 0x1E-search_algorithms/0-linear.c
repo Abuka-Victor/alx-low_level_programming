10c24655ed62f26f2ee1fa6622db98da7e2bc491
Abuka Victor <abukavictoro@gmail.com>
2022-10-03 20:44:36 -0700

    Task 0 betty fix

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -5,10 +5,13 @@
  * @array: the array to be searched
  * @size: The size of the array
  * @value: The value to be found
+ *
+ * Return: an int of the index it finds the number else 0
  */
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
+
 	if (!array)
 		return (-1);
 
